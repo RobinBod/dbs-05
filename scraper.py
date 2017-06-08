3c8a4a00709687fd545f6adb3f0ca921a5c7307a
RobinBod <robinbod@zedat.fu-berlin.de>
2017-06-08 23:17:21 +0200

    change

--- scraper.py
+++ scraper.py
@@ -1,19 +1,21 @@
 From bs4 import BeautifulSoup
 Import requests
-
 PAGECOUNT=10
-Def getPage(url):
-r=requests.get(url)
-data=r.text
-obj=BeautifulSoup(data,”lxml”)
-return obj
-def main():
-words=[]
 
-content=getPage(“https://www.heise.de/thema/https”).findAll(“div”,{“class”:”recommendat
-ion ”})
-for item in content:
-c=item.find(“header”)
-words+=c.contents[0].encode(“utf-8”)
-print (words)
+def getPage(url):
+	r=requests.get(url)
+	data=r.text
+	obj=BeautifulSoup(data,”lxml”)
+	
+	return obj
+
+def main():
+
+
+	words=[]
+	content=getPage(“https://www.heise.de/thema/https”).findAll(“div”,{“class”:”recommendation ”})
+	for item in content:
+		c=item.find(“header”)
+		words+=c.contents[0].encode(“utf-8”)
+	print (words)
 main()
